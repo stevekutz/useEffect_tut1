226ed918bd7a86b00d1bbffd8f44c91989668484
stevekutz <stkutz@gmail.com>
2019-12-24 17:36:20 -0600

    updated to 3:20

--- app/src/App.js
+++ app/src/App.js
@@ -5,24 +5,37 @@ import {useForm} from './comp/useForm';
 
 
 function App() {
-    const [values, handleChange] = useForm({email: '', password: ''});
+    const [values, handleChange] = useForm({email: '', password: '', firstName: ''});
 
     useEffect(() =>{
 
         console.log('useEffect called, render occured');
-    })
+    // , 2nd param is "dependency array", compare var to prevstate verify if if updated, if Y then function called    
+    // CANNOT just put 'values' into dependency array
+    // use }, []); to only call function inside on first render  
+    // use {, [values.passwprd]} only calls when password changes, IGNORES changes to email state   
+    }, [values.password, values.email]); // only calls when password OR emaail changed 
 
     return (
         <Segment>
             <Input
                 name = 'email'
                 value = {values.email}
+                placeholder = 'email'
                 onChange = {handleChange}
             
             />
+            <Input
+                name = 'first name'
+                placeholder = 'first name'
+                value = {values.firstName}
+                onChange = {handleChange}
+        
+            />
             <Input 
-                type = 'pasword'
+                type = 'password'
                 name = 'password'
+                placeholder = 'password'
                 value = {values.password}
                 onChange = {handleChange}
 
