9efbe70f36c37b355e980d749a21b9a501ba251e
stevekutz <stkutz@gmail.com>
2019-12-24 19:20:28 -0600

    created count handler

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,22 @@
 import React, {useEffect} from 'react';
 import {useState} from 'reinspect';
-import {Input, Segment} from 'semantic-ui-react';
+import {Input, Label, Segment} from 'semantic-ui-react';
 import {useForm} from './comp/useForm';
 
 
 function App() {
     const [values, handleChange] = useForm({email: '', password: '', firstName: ''});
+    const [count, incCount] = useState(0, 'Count State');
+
+    const incrementCount = () => {
+        incCount(count + 1);
+    }
 
     useEffect(() =>{
 
         console.log('useEffect called, render occured');
 
-
+        incrementCount();
 
         // cleanup function ==> componentWillUnMount
         return () => {
@@ -26,12 +31,15 @@ function App() {
     // , 2nd param is "dependency array", compare var to prevstate verify if if updated, if Y then function called    
     // CANNOT just put 'values' into dependency array
     // use    }, []); to only call function inside on first render  
-    // use    }, [values.passwprd]} only calls when password changes, IGNORES changes to email state 
+    // use    }, [values.password]} only calls when password changes, IGNORES changes to email state 
     // use   }, [values.password, values.email]); // only calls when password OR email changed  
-    }, []); // 
+    // }, [count]);   starts continuous re-render
+     }, [values.firstName]); // 
 
     return (
         <Segment>
+
+            <Label> Count at : {count.toString()}</Label>
             <Input
                 name = 'email'
                 value = {values.email}
