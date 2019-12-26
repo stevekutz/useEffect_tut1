9b8c577f5d6d9f8c3d39c9077d5ff343ea5341ff
stevekutz <stkutz@gmail.com>
2019-12-25 19:02:50 -0600

    added 3 versions of custom hook using async/await

--- app/src/comp/useFetch.js
+++ app/src/comp/useFetch.js
@@ -7,17 +7,87 @@ export const useFetch = (url) => {
         // NOTE this API is text rather than JSON based
 
         // fetch return a Promise
-        // if successful, returns response(in this case text)
-        // we access the body of the reponse using .text()
-        // this returns another Promise with the data
+        // if successful, returns response obj
+        // we access the body of the response using .text()
+        
+    (async function() {
+        try{
+            let res = await fetch(url);
+            console.log('Promise is: ', res);
+            console.log('status is: ', res.status);
 
-        fetch(url)
-            .then(res =>  {
-                console.log('Promise is: ', res);
-                return res.text();
-            })                
-            .then(data => {
-                console.log('data is: ', data)
-            })
+            let data = await res.text();
+            console.log("data is ", data);
+        } catch (err) {
+            console.log('error is: ', err);
+        }
+        
+    })()
+
     }, [url]);
 };
+
+// export const useFetch = (url) => {
+//     useEffect( () => {
+//         // use .then approach instead of async/await
+//         // NOTE this API is text rather than JSON based
+
+//         // fetch return a Promise
+//         // if successful, returns response obj
+//         // we access the body of the response using .text()
+//         // this returns another Promise with the data
+
+//         fetch(url)
+//             .then(res =>  {
+//                 console.log('Promise is: ', res);
+//                 console.log('status is: ', res.status);
+//                 return res.text();
+//             })                
+//             .then(data => {
+//                 console.log('data is: ', data)
+//             }).catch(err => {
+//                 return err;
+//             })
+//     }, [url]);
+// };
+
+
+// (async function() {
+//     let res = await fetch(url);
+//     console.log('Promise is: ', res);
+//     console.log('status is: ', res.status);
+    
+//     let data = await res.text();
+//     console.log("data is ", data);
+// })()
+
+
+// async function getData () {
+//     try {
+//         let res = await fetch(url);
+//         console.log('Promise is: ', res);
+//         console.log('status is: ', res.status);
+        
+//         let data = await res.text();
+//         console.log("data is ", data);
+//     } catch (err) {
+//         console.log('error is: ', err);    
+//     }
+// }
+
+// getData();
+
+// const getData = async () => {
+//     try {
+//         let res = await fetch(url);
+//         console.log('Promise is: ', res);
+//         console.log('status is: ', res.status);
+        
+//         let data = await res.text();
+//         console.log("data is ", data);
+//     } catch (err) {
+//         console.log('error is: ', err);    
+//     }
+// }
+
+// getData();
